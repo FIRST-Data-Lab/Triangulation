6450d7e1e68655c265948ef0f176ae37bbd99fce
funstatpackages <liwangresearchgroup@gmail.com>
2019-05-30 13:55:52 -0400

    Third try of making "Tr" as numeric instead of list.

--- R/UStri.R
+++
@@ -1,5 +0,0 @@
-install_github("funstatpackages/Triangulation")
-library(Triangulation)
-bb=read.csv("boundusa.csv",head=FALSE)
-VT=TriMesh(bb,6) # rough triangulation
-VT=TriMesh(bb,9) # fine triangulation
